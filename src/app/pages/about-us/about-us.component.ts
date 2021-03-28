686841484079fb837739d31f185b86404ae4bc1c
Nikola Jeremic <nikola87kg@gmail.com>
2021-03-28 19:17:13 +0200

    about us references

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -10,11 +10,43 @@ import { BrandsService } from './../../_services/brands.service';
 export class AboutUsComponent implements OnInit {
 
   brands$: Observable<any>;
+  referenceList = [
+    'Čar',
+    'Joker',
+    'Piramida',
+    'Str Gruja',
+    'Str Braća Popović',
+    'Auto Servis Petrović',
+    'Auto Servis Orlović',
+    'Auto Servis Boško',
+    'Villa 117',
+    'Tempo bela tehnika',
+    'Just Gym',
+    'Agencija Milićević',
+    'Optika Sfera',
+    'InterPrint',
+    'Menjačnica Ortak',
+    'Menjačnica Čavar',
+    'Agro centar Jovanović',
+    'Itali Yu Car',
+    'Matiko Promet',
+    'Auto Lider',
+    'Hellas Centar',
+    'Auto škola Stop',
+    'Auto gas Lukić',
+    'Era Perionica',
+    'Aleksa Termo Frigo'
+  ];
+
   constructor(public brandService: BrandsService) {
     this.getBrands();
   }
 
   ngOnInit(): void {
+  }
+
+  joinReferences(): string {
+    return this.referenceList.join(', ');
   }
 
   getBrands(): void {
